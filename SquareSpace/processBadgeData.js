b10d26e28fd4c33a69c4daafa13af01868ef72f9
David P. Mott <dpmott@gmail.com>
2018-09-22 02:47:03 -0400

    Updating badge processing script

--- SquareSpace/processBadgeData.js
+++ SquareSpace/processBadgeData.js
@@ -10,27 +10,36 @@ const readDir = Promise.promisify(Fs.readdir);
 const readFile = Promise.promisify(Fs.readFile);
 const cache = {};
 
+function readCSV(filename) {
+  const defaultItem = { [LINEITEM_KEY]: filename };
+  
+  return readFile(filename)
+    .then(content => csvParse(content, { columns: true })) // relax_column_count
+    .then(group => cache[(group[0] || defaultItem)[LINEITEM_KEY]] = group);
+}
+
+function generateBadgeNumbers() {
+  let badgeNum = 1;
+  // Assume that all of the entries are sorted by order # already, as they are in the CSV
+  [].concat(...Object.values(cache))
+    .sort((a,b) => a['Order ID'] - b['Order ID'])
+    .map(item => item.badgeNum = badgeNum++);
+
+  // console.log('Badge numbers:', Object.values(cache).map(group => group.map(item => item.badgeNum)));
+  console.log('Total badges sold:', badgeNum);
+}
+
 function printCounts() {
   Object.keys(cache).sort().forEach(key => {
     console.log(`${key}: ${cache[key].length}`);
   });
 }
 
-function readCSV(filename) {
-  return readFile(filename)
-  .then(content => csvParse(content, { columns: true })) // relax_column_count
-  .then(data => data.map(item => {
-    // Organize all data based on the 'Lineitem name' key
-    cache[item[LINEITEM_KEY]] = cache[item[LINEITEM_KEY]] || [];
-    cache[item[LINEITEM_KEY]].push(item);
-  }));
-}
 
 if (process.argv.length < 3) {
   console.error('You must specify a folder path, where the CSV files can be found.');
   process.exit(-1);
 }
-
 
 const folder = process.argv[2];
 console.log(`Reading CSV files from '${folder}'`);
@@ -39,6 +48,9 @@ readDir(folder)
 .then(files => files.filter(file => file.match(/\.csv$/)))
 .then(files => files.map(file => `${folder}/${file}`))
 .then(files => Promise.all(files.map(readCSV)))
+.then(generateBadgeNumbers)
+// TODO: organize packet report based on the FORM email address
+// TODO: generate mail merge input file 
 .then(printCounts)
 .then(() => console.log('Done!'))
 .catch((err) => console.log('Error!', err));
