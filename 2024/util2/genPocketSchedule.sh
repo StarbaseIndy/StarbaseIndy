357cfcaf6d9199315698075b1845871ca42ccb4b
David Mott <dpmott@gmail.com>
2023-12-12 23:05:16 -0500

    Initializing 2024 pocket schedule

---
+++ 2024/util2/genPocketSchedule.sh
@@ -0,0 +1,4 @@
+#!bash -e
+node pocketSchedule.js  --locations "Main Bridge" Engineering Astrophysics "The Lab" "Motley Kids" Communications Promenade "Vic's Place" --exclude "Starship Astraeus" "Myst Brigade Bridge"
+
+
